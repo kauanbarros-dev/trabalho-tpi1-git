59d18c0e7f92c6893b238857fdd6ea5015e27b37
kauanbarros-dev <kauanbarrosvicente@gmail.com>
2026-04-07 20:41:05 -0300

    Adicionando operacoes de subtracao e multiplicacao - branch oresgate

--- Calculadora.java
+++ Calculadora.java
@@ -24,8 +24,16 @@ public class Calculadora {
                 throw new IllegalArgumentException("Operação inválida: " + operacao);
         
 
-        public static int somar(int a, int b) {
+    public static int somar(int a, int b) {
         return a + b;
+
+    public static int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+}
 }
         }
         }
